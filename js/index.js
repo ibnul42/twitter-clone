14b9da33adca91abad70e90d84eca1a55a8618f4
ibnul42 <ibnulashir42@gmail.com>
2021-06-17 22:47:07 +0600

    Full version of this twitter Clone (Lite) completed

--- js/index.js
+++ js/index.js
@@ -1,131 +1,139 @@
 const URL = "http://localhost:3000/tweets";
-// http://localhost:3000/tweets?q=coding&count=10
-/**
- * Retrive Twitter Data from API
- */
+const nextPageData = {
+    loading: false,
+    url: null
+}
 
 const onEnter = (e) => {
-    if(e.key == "Enter"){
+    if(e.key == "Enter") {
         getTwitterData();
     }
 }
 
-const getTwitterData = () => {
+const onNextPage = () => {
+    if(nextPageData.url){
+        getTwitterData(true);
+    }
+}
+
+const getTwitterData = (nextPage=false) => {
     const query = document.getElementById("user-search-input").value;
     if(!query) return;
     const encodedQuery = encodeURIComponent(query);
     const params = `q=${encodedQuery}&result_type=mixed`;
     let fullUrl = `${URL}?${params}`;
-    fetch(fullUrl)
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        buildTweets(data.statuses);
-    })
+    if(nextPage){
+        fullUrl = nextPageData.url;
+        nextPageData.loading = true;
+    }
+    fetch(fullUrl, {
+        method: 'GET'
+    }).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        saveNextPage(data.search_metadata)
+        buildTweets(data.statuses, nextPage);
+        nextPageButtonVisibility(data.search_metadata);
+    });
 }
 
-/**
- * Save the next page data
- */
 const saveNextPage = (metadata) => {
+    nextPageData.url = `${URL}${metadata.next_results}`
+    nextPageData.loading = false;
 }
 
-/**
- * Handle when a user clicks on a trend
- */
 const selectTrend = (e) => {
+    const trendText = e.innerText;
+    document.getElementById("user-search-input").value = trendText;
+    getTwitterData();
 }
 
-/**
- * Set the visibility of next page based on if there is data on next page
- */
 const nextPageButtonVisibility = (metadata) => {
+    let visibility = 'hidden';
+    if(metadata.next_results){
+        visibility = 'visible';
+    }
+    document.getElementById('next-page').style.visibility = visibility;
 }
 
-/**
- * Build Tweets HTML based on Data from API
- */
 const buildTweets = (tweets, nextPage) => {
     let twitterContent = "";
-    tweets.map((tweet) => {
+    tweets.map((tweet)=>{
+        const createdDate = moment(tweet.created_at).fromNow();
         twitterContent += `
             <div class="tweet-container">
                 <div class="tweet-user-info">
-                    <div class="tweet-user-profile">
-                        
-                    </div>
+                    <div class="tweet-user-profile" style="background-image: url(${tweet.user.profile_image_url_https})"></div>
                     <div class="tweet-user-name-container">
-                        <div class="tweet-user-fullname">
-                            Ibnul Ashir
-                        </div>
-                        <div class="tweet-user-username">
-                            @ibnul42
-                        </div>
+                        <div class="tweet-user-fullname">${tweet.user.name}</div>
+                        <div class="tweet-user-username">@${tweet.user.screen_name}</div>
                     </div>
                 </div>
-                `
-                if(tweet.extended_entities 
-                    && tweet.extended_entities.media
-                    && tweet.extended_entities.media.length > 0){
-                    twitterContent += buildImages(tweet.extended_entities.media);
-                    twitterContent += buildVideo(tweet.extended_entities.media);
-                }
-                twitterContent += `
+        `
+        if(tweet.extended_entities 
+            && tweet.extended_entities.media
+            && tweet.extended_entities.media.length > 0){
+            twitterContent += buildImages(tweet.extended_entities.media);
+            twitterContent += buildVideo(tweet.extended_entities.media);
+        }
+        twitterContent += `
                 <div class="tweet-text-container">
                     <span class="tweet-text">
                     ${tweet.full_text}
                     </span>
                 </div>
-                <div class="tweet-date-container">
-                    10 hours ago
+                <div class="tweet-date">
+                    ${createdDate}
                 </div>
             </div>
         `
     })
-    document.querySelector('.tweets-list').innerHTML = twitterContent;
+    if(nextPage){
+        document.querySelector('.tweets-list').insertAdjacentHTML('beforeend', twitterContent)
+    } else {
+        document.querySelector('.tweets-list').innerHTML = twitterContent;
+    }
 }
 
-/**
- * Build HTML for Tweets Images
- */
- const buildImages = (mediaList) => {
+const buildImages = (mediaList) => {
     let imagesContent = `<div class="tweet-images-container">`;
     let imagesExist = false;
     mediaList.map((media)=>{
         if(media.type == "photo"){
             imagesExist = true;
             imagesContent += `
-                <div class="tweet-image" style="background-image: url(${media.media_url_https})"></div>`
+                <div class="tweet-image" style="background-image: url(${media.media_url_https})"></div>
+            `
         }
     })
     imagesContent += `</div>`;
     return (imagesExist ? imagesContent : '');
 }
 
-/**
- * Build HTML for Tweets Video
- */
 const buildVideo = (mediaList) => {
     let videoContent = `<div class="tweet-video-container">`;
-    let videoExist = false;
+    let videoExists = false;
     mediaList.map((media)=>{
-        if(media.type == "video"){
-            videoExist = true;
+        if(media.type == "video" || media.type == 'animated_gif'){
+            videoExists = true;
+            const video = media.video_info.variants.find((video)=>video.content_type == 'video/mp4');
+            const videoOptions = getVideoOptions(media.type);
             videoContent += `
-            <video controls>
-                <source src="${media.video_info.variants[0].url}" type="video/mp4">
-            </video>
-            `
-        } else if(media.type == "animated_gif"){
-            videoExist = true;
-            videoContent += `
-            <video loop autoplay>
-                <source src="${media.video_info.variants[0].url}" type="video/mp4">
+            <video ${videoOptions}>
+                <source src="${video.url}" type="video/mp4">
+                Your browser does not support HTML5 video.
             </video>
             `
         }
     })
     videoContent += `</div>`;
-    return (videoExist ? videoContent : '');
+    return (videoExists ? videoContent : '');
+}
+
+const getVideoOptions = (mediaType) => {
+    if(mediaType == 'animated_gif'){
+        return "loop autoplay";
+    } else {
+        return "controls";
+    }
 }
